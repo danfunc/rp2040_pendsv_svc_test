ee83494298152d5f05486c316a2ecaec01b7577b
danfuncl <113231660+danfunc@users.noreply.github.com>
2024-05-28 01:07:12 +0000

    アセンブラを含めてコンテキストスイッチの完成

---
+++ context_switch.s
@@ -0,0 +1,40 @@
+
+
+.cpu cortex-m0plus
+.thumb
+.global pendsv_handler
+.type   pendsv_handler, %function
+pendsv_handler:
+bl get_current_context;
+str r4, [r0,#0]
+str r5, [r0,#4]
+str r6, [r0,#8]
+str r7, [r0,#12]
+mov r1, r8
+str r1, [r0,#16]
+mov r1, r9
+str r1, [r0,#20]
+mov r1, r10
+str r1, [r0,#24]
+mov r1, r11
+str r1, [r0,#28]
+mrs r1, PSP
+str r1, [r0,#32]
+bl pop_next_context;
+ldr r4, [r0,#0]
+ldr r5, [r0,#4]
+ldr r6, [r0,#8]
+ldr r7, [r0,#12]
+ldr r1, [r0,#16]
+mov r8, r1
+ldr r1, [r0,#20]
+mov r9, r1
+ldr r1, [r0,#24]
+mov r10, r9
+ldr r1, [r0,#28]
+mov r11, r1
+ldr r1, [r0,#32]
+msr PSP, r1
+mov r0, #2
+neg r0,r0
+bx r0
--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 #include <cinttypes>
-#include <exception>
 #include <hardware/exception.h>
 #include <memory>
 #include <pico/stdio.h>
 #include <pico/stdlib.h>
+#include <queue>
 #include <stdio.h>
 #include <stdlib.h>
 using method = void (*)(uintptr_t, uintptr_t, uintptr_t, uintptr_t);
@@ -33,12 +33,12 @@ void trap() {
   }
 }
 int main(int argc, char const *argv[]);
+void context_switch() __attribute__((__noinline__));
 
 struct context {
   uint32_t r4 = 0, r5 = 0, r6 = 0, r7 = 0, r8 = 0, r9 = 0, r10 = 0, r11 = 0;
-  void const *stack_start;
   void *sp;
-  exec_mode mode = exec_mode::thread_MSP;
+  void const *stack_start;
   context(method method, size_t arg0, size_t arg1, size_t arg2, size_t arg3,
           size_t stack_size = 1024)
       : stack_start{malloc(stack_size + sizeof(exception_stack))} {
@@ -54,68 +54,172 @@ struct context {
       sp = (void *)(((uintptr_t)stack_start + stack_size) & ~0xfL);
       *(exception_stack *)sp = return_stack;
     } else
-      ;
+      sp = nullptr;
   };
   context() : stack_start{nullptr} {};
   ~context() {
     if (stack_start != nullptr)
       free((void *)stack_start);
   };
+
+  void __always_inline save_from_r4_to_r11() {
+    uint32_t r4, r5, r6, r7, r8, r9, r10, r11, r12;
+    asm volatile("mov %0, r4" : "=r"(r4));
+    this->r4 = r4;
+    asm volatile("mov %0, r5" : "=r"(r5));
+    this->r5 = r5;
+    asm volatile("mov %0, r6" : "=r"(r6));
+    this->r6 = r6;
+    asm volatile("mov %0, r7" : "=r"(r7));
+    this->r7 = r7;
+    asm volatile("mov %0, r8" : "=r"(r8));
+    this->r8 = r8;
+    asm volatile("mov %0, r9" : "=r"(r9));
+    this->r9 = r9;
+    asm volatile("mov %0, r10" : "=r"(r10));
+    this->r10 = r10;
+    asm volatile("mov %0, r11" : "=r"(r11));
+    this->r11 = r11;
+  };
+
+  void __always_inline load_from_r4_to_r11() {
+    uint32_t r4, r5, r6, r7, r8, r9, r10, r11, r12;
+    this->r4 = r4;
+    asm volatile("mov r4, %0" ::"r"(r4));
+    this->r5 = r5;
+    asm volatile("mov r5, %0" ::"r"(r5));
+    this->r6 = r6;
+    asm volatile("mov r6, %0" ::"r"(r6));
+    this->r7 = r7;
+    asm volatile("mov r7, %0" ::"r"(r7));
+    this->r8 = r8;
+    asm volatile("mov r8, %0 " ::"r"(r8));
+    this->r9 = r9;
+    asm volatile("mov r9, %0 " ::"r"(r9));
+    this->r10 = r10;
+    asm volatile("mov r10, %0" ::"r"(r10));
+    this->r11 = r11;
+    asm volatile("mov r11, %0" ::"r"(r11));
+  };
+};
+
+extern "C" {
+context *get_current_context() __noinline;
+context *pop_next_context() __noinline;
+context *load_from_r4_to_r11(context *t);
+context *save_from_r4_to_r11(context *t);
+void pendsv_handler(void);
+}
+
+context *save_from_r4_to_r11(context *t) {
+  uint32_t r4, r5, r6, r7, r8, r9, r10, r11, r12;
+  asm volatile("mov %0, r4" : "=r"(r4));
+  t->r4 = r4;
+  asm volatile("mov %0, r5" : "=r"(r5));
+  t->r5 = r5;
+  asm volatile("mov %0, r6" : "=r"(r6));
+  t->r6 = r6;
+  asm volatile("mov %0, r7" : "=r"(r7));
+  t->r7 = r7;
+  asm volatile("mov %0, r8" : "=r"(r8));
+  t->r8 = r8;
+  asm volatile("mov %0, r9" : "=r"(r9));
+  t->r9 = r9;
+  asm volatile("mov %0, r10" : "=r"(r10));
+  t->r10 = r10;
+  asm volatile("mov %0, r11" : "=r"(r11));
+  t->r11 = r11;
+  return t;
+};
+
+context *load_from_r4_to_r11(context *t) {
+  uint32_t r4, r5, r6, r7, r8, r9, r10, r11, r12;
+  t->r4 = r4;
+  asm volatile("mov r4, %0" ::"r"(r4));
+  t->r5 = r5;
+  asm volatile("mov r5, %0" ::"r"(r5));
+  t->r6 = r6;
+  asm volatile("mov r6, %0" ::"r"(r6));
+  t->r7 = r7;
+  asm volatile("mov r7, %0" ::"r"(r7));
+  t->r8 = r8;
+  asm volatile("mov r8, %0 " ::"r"(r8));
+  t->r9 = r9;
+  asm volatile("mov r9, %0 " ::"r"(r9));
+  t->r10 = r10;
+  asm volatile("mov r10, %0" ::"r"(r10));
+  t->r11 = r11;
+  asm volatile("mov r11, %0" ::"r"(r11));
+  return t;
 };
 
 class cpu_manager;
 
 int main(int argc, const char *argv[]);
-
+void cpp_context_switch();
 class rp2040 {
   friend cpu_manager;
+  friend context *get_current_context();
+  friend context *pop_next_context();
+  friend void context_switch();
+  friend void cpp_context_switch();
   friend int main(int, const char *[]);
-  static std::shared_ptr<context> current_contexts[2];
+  inline static context *get_current_context();
+  inline static context *pop_next_context();
   static cpu_manager *cpu_managers[2];
-  static void pendsv_handler();
   rp2040(cpu_manager &manager, size_t core_num);
 };
+
+struct task;
 
 class cpu_manager {
   friend rp2040;
   friend int main(int, const char **);
-  std::shared_ptr<context> pop_next_context();
+  context *pop_next_context();
+  context *get_current_context();
+  std::shared_ptr<context> current_context, default_context;
   rp2040 driver;
-  cpu_manager(size_t core_num) : driver{*this, core_num} {};
+  cpu_manager(size_t core_num) : driver{*this, core_num} {
+    current_context = std::make_shared<context>();
+    default_context = std::make_shared<context>((method)target, 0, 0, 0, 0);
+  };
 };
 
 cpu_manager *rp2040::cpu_managers[2] = {nullptr, nullptr};
-std::shared_ptr<context> rp2040::current_contexts[2] = {
-    {std::make_shared<context>()}, {std::make_shared<context>()}};
 
 rp2040::rp2040(cpu_manager &manager, size_t core_num) {
-  cpu_managers[core_num] = &manager;
-}
-
-std::shared_ptr<context> cpu_manager::pop_next_context() {
-  return std::make_shared<context>((method)target, 0, 0, 0, 0);
-}
-
-void rp2040::pendsv_handler() {
-  int core_num = get_core_num();
-  if (current_contexts[core_num]) {
+  if (cpu_managers[core_num] == nullptr) {
+    cpu_managers[core_num] = &manager;
   }
-  current_contexts[core_num] = cpu_managers[core_num]->pop_next_context();
-  void *sp = current_contexts[core_num]->sp;
-  asm volatile("msr PSP,%0" ::"r"(sp));
-  exc_return return_code = exc_return::thread_PSP;
-  asm volatile("bx %0" ::"r"(return_code));
 }
+
+context *rp2040::get_current_context() {
+  return cpu_managers[get_core_num()]->get_current_context();
+}
+
+context *rp2040::pop_next_context() {
+  return cpu_managers[get_core_num()]->pop_next_context();
+}
+
+context *cpu_manager::get_current_context() { return current_context.get(); }
+
+context *cpu_manager::pop_next_context() {
+  current_context = default_context;
+  return current_context.get();
+}
+
+context *get_current_context() { return rp2040::get_current_context(); };
+context *pop_next_context() { return rp2040::pop_next_context(); };
+
 int main(int argc, char const *argv[]) {
+  exc_return a = exc_return::thread_PSP;
   stdio_init_all();
   cpu_manager manager{0};
-  // asm volatile("msr CONTROL, %0" ::"r"(SP_SEL_VAL));
-  exception_set_exclusive_handler(PENDSV_EXCEPTION, rp2040::pendsv_handler);
+  exception_set_exclusive_handler(PENDSV_EXCEPTION, pendsv_handler);
   while (1) {
     *(uintptr_t *)(PPB_BASE + M0PLUS_ICSR_OFFSET) |= 1 << 28;
     sleep_ms(1000);
     printf("pendsv_returnd\n");
   }
-
   return 0;
 }
